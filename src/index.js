6bc6657b7884c9ee6c8fcf8aebc1cbb3dde0fc82
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-07-16 20:35:16 +0200

    update

--- src/index.js
+++ src/index.js
@@ -44,31 +44,35 @@ const fetchImg = async () => {
     downloads: image.downloads,
   }));
 
+  return images;
+};
+
+function renderImg() {
   const markup = images
     .map(images => {
       return `<div class="photo-card">
-    <img src="${images.webformatURL}" alt="${images.tags}" loading="lazy" class="img"/>
-    <div class="info">
-      <p class="info-item">
-        <b>${images.likes}: Likes</b>
-      </p>
-      <p class="info-item">
-        <b>${images.views}: Views</b>
-      </p>
-      <p class="info-item">
-        <b>${images.comments}: Comments</b>
-      </p>
-      <p class="info-item">
-        <b>${images.downloads}: Downloads</b>
-      </p>
-    </div>
-  </div>`;
+  <img src="${images.webformatURL}" alt="${images.tags}" loading="lazy" class="img"/>
+  <div class="info">
+    <p class="info-item">
+      <b>${images.likes}: Likes</b>
+    </p>
+    <p class="info-item">
+      <b>${images.views}: Views</b>
+    </p>
+    <p class="info-item">
+      <b>${images.comments}: Comments</b>
+    </p>
+    <p class="info-item">
+      <b>${images.downloads}: Downloads</b>
+    </p>
+  </div>
+</div>`;
     })
     .join('');
 
   gallery.innerHTML = markup;
-  return images;
-};
+  return gallery;
+}
 
 function clearGallery(data) {
   if (data.hits.length === 0) {
@@ -84,9 +88,11 @@ form.addEventListener('submit', event => {
   fetchImg(searchPhoto);
   searchPhoto = '';
   loadMoreButton.style.display = 'block';
-  return;
 });
 
 loadMoreButton.addEventListener('click', () => {
   page++;
+  fetchImg(searchPhoto);
+  searchPhoto = '';
+  return;
 });
